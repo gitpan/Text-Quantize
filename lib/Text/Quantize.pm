ef0e4a3ac64a43d14b0451fc5ee1291cb382a3c0
Shawn M Moore <cpan@sartak.org>
2012-11-14 19:37:30 +0000

    Import of SARTAK/Text-Quantize-0.03 from CPAN.
    gitpan-cpan-distribution: Text-Quantize
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         SARTAK/Text-Quantize-0.03.tar.gz
    gitpan-cpan-author:       SARTAK
    gitpan-cpan-maturity:     released

--- lib/Text/Quantize.pm
+++ lib/Text/Quantize.pm
@@ -6,6 +6,10 @@ use List::Util 'sum';
 
 sub bucketize {
     my $elements = shift;
+    my $options  = {
+        add_endpoints => 1,
+        %{ shift(@_) || {} },
+    };
 
     my %buckets;
     for my $element (@$elements) {
@@ -22,6 +26,19 @@ sub bucketize {
         }
 
         $buckets{$bucket}++;
+    }
+
+    # allow user to specify only one of the two endpoints if desired, and figure the other out
+    if ($options->{add_endpoints}) {
+        unless (defined($options->{minimum}) && defined($options->{maximum})) {
+            my ($min, $max) = _endpoints_for(\%buckets);
+            $options->{minimum} = $min if !defined($options->{minimum});
+            $options->{maximum} = $max if !defined($options->{maximum});
+        }
+
+        # force the min and max to exist, but if that endpoint has a value don't blindly overwrite it
+        $buckets{ $options->{minimum} } ||= 0;
+        $buckets{ $options->{maximum} } ||= 0;
     }
 
     return \%buckets;
@@ -64,24 +81,6 @@ sub _endpoints_for {
     return ($min_endpoint, $max_endpoint);
 }
 
-sub _massage_buckets {
-    my $buckets = shift;
-    my $options = shift;
-
-    # allow user to specify only one of the two endpoints if desired, and figure the other out
-    unless (defined($options->{minimum}) && defined($options->{maximum})) {
-        my ($min, $max) = _endpoints_for($buckets);
-        $options->{minimum} = $min if !defined($options->{minimum});
-        $options->{maximum} = $max if !defined($options->{maximum});
-    }
-
-    # force the min and max to exist, but if that endpoint has a value don't blindly overwrite it
-    $buckets->{ $options->{minimum} } ||= 0;
-    $buckets->{ $options->{maximum} } ||= 0;
-
-    return $buckets;
-}
-
 sub quantize {
     my $elements = shift;
     my %options  = (
@@ -93,7 +92,7 @@ sub quantize {
         %{ shift(@_) || {} },
     );
 
-    my $buckets = _massage_buckets(bucketize($elements, \%options));
+    my $buckets = bucketize($elements, \%options);
 
     my $distribution_width = $options{distribution_width};
     my $distribution_character = $options{distribution_character};
@@ -147,22 +146,124 @@ Text::Quantize - render a list of numbers as a textual chart
 
 =head1 VERSION
 
-version 0.02
+version 0.03
 
 =head1 SYNOPSIS
 
     use Text::Quantize;
-
+    
     print quantize([26, 24, 51, 77, 21]);
-
+    
     __END__
-
+    
      value  ------------- Distribution ------------- count
          8 |                                         0
         16 |@@@@@@@@@@@@@@@@@@@@@@@@                 3
         32 |@@@@@@@@                                 1
         64 |@@@@@@@@                                 1
        128 |                                         0
+
+
+
+    use Text::Quantize ();
+
+    print Text::Quantize::quantize([map { chomp; $_ } <DATA>], {
+        left_label             => 'microseconds',
+        middle_label           => 'Calls per time bucket',
+        right_label            => 'syscalls',
+        distribution_width     => 80,
+        distribution_character => '=',
+    });
+    
+    __END__
+    
+     microseconds  ---------------------------- Calls per time bucket ----------------------------- syscalls
+              256 |                                                                                 0
+              512 |====                                                                             5
+             1024 |=====                                                                            7
+             2048 |==================                                                               23
+             4096 |============================                                                     36
+             8192 |=======                                                                          9
+            16384 |=                                                                                2
+            32768 |                                                                                 1
+           262144 |                                                                                 1
+           524288 |                                                                                 1
+          1048576 |                                                                                 1
+          2097152 |=======                                                                          9
+          4194304 |===                                                                              4
+          8388608 |                                                                                 1
+         16777216 |                                                                                 0
+
+=head1 FUNCTIONS
+
+=head2 C<quantize([integers], {options})>
+
+C<quantize> takes an array reference of integers and an optional
+hash reference of options, and produces a textual histogram of the
+integers bucketed into powers-of-2 sets.
+
+Options include:
+
+=over 4
+
+=item C<left_label> (default: C<value>)
+
+Controls the text of the left-most label which represents the
+bucket's contents.
+
+=item C<middle_label> (default: C<Distribution>)
+
+Controls the text of the middle label which can be used to title
+the histogram.
+
+=item C<right_label> (default: C<count>)
+
+Controls the text of the right-most label which represents how many
+items are in that bucket.
+
+=item C<distribution_width> (default: C<40>)
+
+Controls how many characters wide the textual histogram is. This
+does not include the legends.
+
+=item C<distribution_character> (default: C<@>)
+
+Controls the character used to represent the data in the histogram.
+
+=item C<add_endpoints> (default: C<1>)
+
+Controls whether the top and bottom lines (which are going to have
+values of 0) are added. They're included by default because it hints
+that the data set is complete.
+
+=back
+
+=head2 C<bucketize([integers], {options})>
+
+C<bucketize> takes an array reference of integers and an optional
+hash reference of options, and produces a hash reference of those
+integers bucketed into powers-of-2 sets.
+
+Options include:
+
+=over 4
+
+=item add_endpoints (default: C<1>)
+
+Controls whether extra buckets, smaller than the minimum value and
+larger than the maximum value, (which are going to have values of
+0) are added. They're included by default because it hints that the
+data set is complete.
+
+=back
+
+=head1 SEE ALSO
+
+C<DTrace>, which is where I first saw this kind of C<quantize()>
+histogram.
+
+L<dip>, which ported C<quantize()> to Perl first, and from which I
+took a few insights.
 
 =head1 AUTHOR
 
